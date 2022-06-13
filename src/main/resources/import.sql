4764e6ef61687121a13894b17c1167ef47d77dd6
Teo1121 <teo.ferenac@gmail.com>
2022-06-13 14:26:15 +0200

    postgresql fix

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -12,11 +12,11 @@ INSERT INTO professors(name,details) VALUES('Jozo Hodžić','Odlican, idealan pr
 INSERT INTO professors(name,details) VALUES('Mirjana Božić','Zna sta radi, ali nezna kako objasnit');
 INSERT INTO professors(name,details) VALUES('Radmilo Crnčević','Nezna nista');
 
-INSERT INTO reviews(score,comment,anonymous,professor_id,user_id) VALUES(5,'Stvarno odlican, bez rijeci',0,1,1);
-INSERT INTO reviews(score,comment,anonymous,professor_id,user_id) VALUES(5,'',0,1,1);
+INSERT INTO reviews(score,comment,anonymous,professor_id,user_id) VALUES(5,'Stvarno odlican, bez rijeci',false,1,1);
+INSERT INTO reviews(score,comment,anonymous,professor_id,user_id) VALUES(5,'',false,1,1);
 
-INSERT INTO reviews(score,comment,anonymous,professor_id,user_id) VALUES(4,'Ok, ima puno gorih',0,2,1);
-INSERT INTO reviews(score,comment,anonymous,professor_id,user_id) VALUES(3,'Full me zbunila s gradivom',1,2,1);
+INSERT INTO reviews(score,comment,anonymous,professor_id,user_id) VALUES(4,'Ok, ima puno gorih',false,2,1);
+INSERT INTO reviews(score,comment,anonymous,professor_id,user_id) VALUES(3,'Full me zbunila s gradivom',true,2,1);
 
-INSERT INTO reviews(score,comment,anonymous,professor_id,user_id) VALUES(2,'Doista nezna nista',0,3,1);
-INSERT INTO reviews(score,comment,anonymous,professor_id,user_id) VALUES(1,'Uistinu, nista nezna',1,3,1);
+INSERT INTO reviews(score,comment,anonymous,professor_id,user_id) VALUES(2,'Doista nezna nista',false,3,1);
+INSERT INTO reviews(score,comment,anonymous,professor_id,user_id) VALUES(1,'Uistinu, nista nezna',true,3,1);
